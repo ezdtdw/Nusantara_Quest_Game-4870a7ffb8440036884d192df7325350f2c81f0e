de48964a8cf7e228f3295b8f360aa275069caabc
FahranZandi <162786907+FahranZandi@users.noreply.github.com>
2025-04-20 19:23:52 +0800

    update14

--- Assets/Script/camera/ParallaxEffect.cs
+++ Assets/Script/camera/ParallaxEffect.cs
@@ -1,56 +1,26 @@
 using UnityEngine;
 
-public class ParallaxBackground : MonoBehaviour
+public class ParallaxEffect : MonoBehaviour
 {
-    public Transform cam;
-    public float parallaxEffect;
-    public GameObject backgroundPrefab;
-
-    private float length;
-    private Vector3 lastCamPos;
-    private GameObject leftCopy, rightCopy;
+    private float length, startpos;
+    public GameObject cam;
+    public float parallaxEffect; // Semakin kecil, semakin lambat geraknya (background)
 
     void Start()
     {
-        lastCamPos = cam.position;
+        startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
-
-        // Inisialisasi copy awal
-        CreateCopy(1);  // kanan
-        CreateCopy(-1); // kiri
     }
 
     void Update()
     {
-        float deltaX = cam.position.x - lastCamPos.x;
-        transform.position += Vector3.right * (deltaX * parallaxEffect);
-        lastCamPos = cam.position;
+        float temp = (cam.transform.position.x * (1 - parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
 
-        // Tambah background di kanan jika kamera mendekat ke ujung kanan
-        if (cam.position.x > rightCopy.transform.position.x - (length / 2))
-        {
-            Destroy(leftCopy);
-            leftCopy = gameObject;
-            gameObject.transform.position = rightCopy.transform.position;
-            rightCopy = CreateCopy(1);
-        }
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
 
-        // Tambah background di kiri jika kamera mendekat ke ujung kiri
-        if (cam.position.x < leftCopy.transform.position.x + (length / 2))
-        {
-            Destroy(rightCopy);
-            rightCopy = gameObject;
-            gameObject.transform.position = leftCopy.transform.position;
-            leftCopy = CreateCopy(-1);
-        }
-    }
-
-    GameObject CreateCopy(int direction)
-    {
-        Vector3 spawnPos = transform.position + Vector3.right * length * direction;
-        GameObject copy = Instantiate(backgroundPrefab, spawnPos, Quaternion.identity);
-        copy.transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z); // Mirror
-
-        return copy;
+        // Looping parallax jika infinite scrolling
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
