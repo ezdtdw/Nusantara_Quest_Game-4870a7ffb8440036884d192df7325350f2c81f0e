24d6e10111a2e3affeafa3e41920b88a1c3175ab
FahranZandi <162786907+FahranZandi@users.noreply.github.com>
2025-04-29 10:13:15 +0800

    update 31

--- Assets/Script/character/PlayerHealthBar.cs
+++ Assets/Script/character/PlayerHealthBar.cs
@@ -1,6 +1,29 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerHealthBar
+public class PlayerHealthBar : MonoBehaviour
 {
-    
+    public int maxHealth = 100;
+    public int currentHealth;
+    public HealthBarScript healthBar; // Reference to the HealthBar script
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth); // Set the maximum health in the health bar
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // Simulate taking damage
+        {
+            TakeDamage(10);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
 }
